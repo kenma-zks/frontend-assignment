8bbac232eb3dffcbc63c720271e1a9920dba8bbe
kenma-zks <pallawmgr20@gmail.com>
2023-07-06 14:27:45 +0545

    fix build error

--- onlinestore/src/store/searchSlice.tsx
+++ onlinestore/src/store/searchSlice.tsx
@@ -4,7 +4,7 @@ const searchSlice = createSlice({
   name: "search",
   initialState: "",
   reducers: {
-    setSearchQuery: (state, action) => {
+    setSearchQuery: (_, action) => {
       return action.payload;
     },
   },
